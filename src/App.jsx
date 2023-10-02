3625718502c3e335131205a0cb1e4563c284e5d9
segunojo1 <olusegunkd2017@gmail.com>
2023-10-02 18:15:17 +0100

    assistant

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Segun's On page Assistant</h1>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <img src="https://gifdb.com/images/high/animated-winter-days-walking-xizdn42xi95y262e.gif" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
